07011b845e91cf24f9c2fb8f2e005d0a5f06dffa
Chinnatip Taemkaeo <chinnatip@me.com>
2016-12-25 03:56:08 +0700

    add vuejs script => say hello VUE <3

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+//= require jquery
+//= require jquery_ujs
 //= require vue
 //= require vue-router
 //= require vue-resource
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
 //= require_tree .
