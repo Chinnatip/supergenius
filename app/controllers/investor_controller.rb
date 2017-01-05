b820a11ad1d0724f4a7a7ca650fe0c753a439b33
Chinnatip Taemkaeo <chinnatip@me.com>
2017-01-05 10:00:02 +0700

    add investor comp#3

--- app/controllers/investor_controller.rb
+++ app/controllers/investor_controller.rb
@@ -8,18 +8,46 @@ class InvestorController < ApplicationController
   def financial
   end
 
+  # share group
   def share
   end
+  def share_right
+  end
+  def share_history
+  end
+  def share_holder
+  end
 
+  #bond group
   def bond
   end
+  def bond_policy
+  end
 
+  #stock group
   def stock
   end
-
-  def news
+  def stock_history
+  end
+  def stock_cap
+  end
+  def stock_concensus
+  end
+  def stock_analyst
   end
 
+
+  #news group
+  def news
+  end
+  def news_set
+  end
+  def news_calendar
+  end
+
+  #contact group
   def contact
+  end
+  def contact_email
   end
 end
--- public/js/investor.js
+++ public/js/investor.js
@@ -1,3 +1,16 @@
+Vue.component('blogHeader',{
+  template: `
+  <section class="is-medium _body_header">
+    <div class="hero-body container">
+      <div class="__body">
+        <h1>{{data}}</h1>
+      </div>
+    </div>
+  </section>
+  `,
+  props: ['data']
+});
+
 Vue.component('blogJumbo',{
   template: `
   <section class="is-medium _body_jumbo">
@@ -45,4 +58,70 @@ Vue.component('blogQuote',{
     </section>
   `,
   props: ['stock']
+});
+
+Vue.component('tableRating',{
+  template: `
+  <section class="is-medium _body_table">
+    <div class="right _content">
+      เลือกปีที่ต้องการค้นหา
+      <select class="select">
+        <option value="" v-for="s in selector">{{s}}</option>
+      </select>
+    </div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th v-for="l in table.thead">{{l.title}}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="td in table.tbody">
+          <td>{{td.content[0]}}</td>
+          <td>{{td.content[1]}}</td>
+          <td>{{td.content[2]}}</td>
+          <td>{{td.content[3]}}</td>
+          <td><a :href="td.content[4]" class="button is-primary is-outlined">ดาวโหลด</a></td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr>
+          <th v-for="l in table.tfoot">{{l.title}}</th>
+        </tr>
+      </tfoot>
+    </table>
+  </section>
+  `,
+  props: [ 'table' , 'selector']
+});
+
+Vue.component('tableBlog',{
+  template: `
+  <section class="is-medium _body_table">
+    <div class="_content">
+      ณ วันที่ 29 พฤศจิกายน 2559
+    </div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th v-for="l in table.thead">{{l.title}}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(td,index) in table.tbody">
+          <td>{{index + 1}}</td>
+          <td>{{td.content[0]}}</td>
+          <td>{{td.content[1]}}</td>
+          <td>{{td.content[2]}}</td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr>
+          <th v-for="l in table.tfoot">{{l.title}}</th>
+        </tr>
+      </tfoot>
+    </table>
+  </section>
+  `,
+  props: [ 'table' ]
 });
