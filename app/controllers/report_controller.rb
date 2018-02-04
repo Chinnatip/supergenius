b268800ce1781698ea91774dd401fb04a558d8f6
Chinnatip Taemkaeo <chinnatip@me.com>
2018-02-04 22:02:17 +0700

    fixed min

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -101,7 +101,7 @@ class ReportController < ApplicationController
               puts score_container
               score_result[st.to_sym] = {
                 max: score_container.max_by{|k| k[st].to_i }[st] || 0,
-                min: score_container.min_by{|k| k[st].to_i }[st] || 0,
+                min: score_container.min_by{|k| k[st].to_i }[st],
                 get: score_get[st].to_i || 0
               }
               point_get << score_result[st.to_sym][:get]
