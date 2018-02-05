eed2cead5a4c9010dc2cce8134f772296c7aff92
Chinnatip Taemkaeo <chinnatip@me.com>
2018-02-05 08:55:50 +0700

    fix#11

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -91,16 +91,9 @@ class ReportController < ApplicationController
             score_setter = JSON.parse(classroom.max_score).keys
             score_setter.each do |st|
               #
-              series = []
-              score_container.each do |sc|
-                if sc[st].present?
-                  series << sc[st]
-                end
-              end
-              #
               score_result[st.to_sym] = {
-                max: series.max ,  #score_container.max_by{|k| k[st].to_i }[st] || 0,
-                min: 0 ,  #score_container.min_by{|k| if k[st].present? then k[st].to_i end  }[st] || 0,
+                max: score_container.max_by{|k| k[st].to_i }[st] || 0,
+                min: score_container.min_by{|k| k[st].to_i }[st] || 0,
                 get: score_get[st].to_i || 0
               }
               point_get << score_result[st.to_sym][:get]
